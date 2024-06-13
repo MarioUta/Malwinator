e605e9bfd88a5c069b778e407642c5c72e5574dc
Mario Uță <marioernest818@gmail.com>
2024-06-13 23:03:58 +0300

    Updated camera sender

--- sender.py
+++ sender.py
@@ -1,25 +1,58 @@
-import requests
 import cv2
+import socketio
+import base64
+import sys
+import requests
+
+# Initialize Socket.IO client
+http_session = requests.Session()
+http_session.verify = False
+sio = socketio.Client(http_session=http_session)
+
+@sio.event
+def connect():
+    print('Connected to server')
+
+@sio.event
+def disconnect():
+    print('Disconnected from server')
 
 
 def send_video_to_server():
+    if len(sys.argv) != 2:
+        exit()
+    # Connect to the Flask server
     cap = cv2.VideoCapture(0)
-    url = 'http://192.168.0.146:3000/upload'
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        # Encode the frame (you might need to use a proper encoder)
-        _, encoded_frame = cv2.imencode('.jpg', frame)
-        files = {'file': encoded_frame.tobytes()}
-        response = requests.post(url, files=files, verify=False)
-        if response.status_code != 200:
-            print("Failed to upload frame")
-            break
+    while True:
+        try:
+            sio.connect(sys.argv[1])
+        except:
+            continue
+        connections = 0
+
+        while True:
+            # Capture frame from camera
+            ret, frame = cap.read()
+
+            # Encode frame as JPEG
+            _, encoded_frame = cv2.imencode('.jpg', frame)
+            jpeg_data = base64.b64encode(encoded_frame.tobytes()).decode('utf-8')
+
+            # Send JPEG data to the server
+            try:
+                sio.emit('stream', jpeg_data)
+                print("Emited Stream")
+            except:
+                break
+
+            connections += 1
+            # Break the loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        sio.disconnect()
+
+    # Release the camera and disconnect from the server
     cap.release()
 
 if __name__ == "__main__":
-    try:
-        send_video_to_server()
-    except:
-        pass
+    send_video_to_server()
